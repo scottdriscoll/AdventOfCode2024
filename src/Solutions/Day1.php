1bcb195d4fc7d70a1c00b6b340ca76417d7b8372
Scott Driscoll <scott.driscoll08@gmail.com>
2024-12-01 14:30:53 -0800

    Slight refactoring

---
+++ src/Solutions/Day1.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Solutions;
+
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+
+#[AsTaggedItem('day-1', 50)]
+class Day1 implements SolutionInterface
+{
+    public function part1(string $input): int
+    {
+        $firstArray = [];
+        $secondArray = [];
+        $answer = 0;
+
+        foreach (explode("\n", $input) as $line) {
+            sscanf($line, "%d   %d", $first, $second);
+            $firstArray[] = $first;
+            $secondArray[] = $second;
+        }
+
+        sort($firstArray);
+        sort($secondArray);
+
+        $total = count($firstArray);
+        for ($i = 0; $i < $total; $i++) {
+            $answer += abs($firstArray[$i] - $secondArray[$i]);
+        }
+
+        return $answer;
+    }
+
+    public function part2(string $input): int
+    {
+        $firstArray = [];
+        $secondArray = [];
+        $answer = 0;
+
+        foreach (explode("\n", $input) as $line) {
+            sscanf($line, "%d   %d", $first, $second);
+            $firstArray[] = $first;
+            $secondArray[$second] = isset($secondArray[$second]) ? $secondArray[$second] + 1 : 1;
+        }
+
+        foreach ($firstArray as $val) {
+            if (isset($secondArray[$val])) {
+                $answer += $val * $secondArray[$val];
+            }
+        }
+
+        return $answer;
+    }
+}
