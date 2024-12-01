f16f09a5575489590638b36f9cabbb564d9937c3
Scott Driscoll <scott.driscoll08@gmail.com>
2024-12-01 11:13:16 -0800

    Day 1 Part 2

--- tests/Day1Test.php
+++ tests/Day1Test.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use App\Solutions\Day1\Part1;
+use App\Solutions\Day1\Part2;
 use PHPUnit\Framework\TestCase;
 
 class Day1Test extends TestCase
@@ -12,5 +13,12 @@ class Day1Test extends TestCase
         $part1 = new Part1();
         $input = file_get_contents(__DIR__ . '/fixtures/day1/part1.txt');
         $this->assertSame('11', $part1->run($input));
+    }
+
+    public function testPart2(): void
+    {
+        $part2 = new Part2();
+        $input = file_get_contents(__DIR__ . '/fixtures/day1/part2.txt');
+        $this->assertSame('31', $part2->run($input));
     }
 }
